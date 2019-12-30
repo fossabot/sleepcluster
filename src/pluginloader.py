22feb216c2eca66465e700bb2003e85d23dd6332
Jeffrey Wang <jeffreyc.wang@mail.utoronto.ca>
2019-12-30 18:24:04 -0500

    Reorgnize to distinguish plugin types

--- src/pluginloader.py
+++ src/pluginloader.py
@@ -1,19 +1,15 @@
 import sys, os
 from importlib.machinery import SourceFileLoader
 
-# Easy reading of plugins installed - not actually used
-plugins = [
-			'smrMAT',
-			'processor1',
-			'parameters1'
-		]
 
 # Load all plugins specified
 #	To install a plugin, add the following before after line 11
 #		modules.append(getattr(SourceFileLoader("[pluginName]", "./plugins/[pluginName].py").load_module(), "[pluginName]"))
 def loadPlugins():
-	modules = []
-	modules.append(SourceFileLoader("smrMATreader", "./dataobjects/smrMATreader.py").load_module())
-	modules.append(SourceFileLoader("processor1", "./processors/processor1.py").load_module())
-	modules.append(SourceFileLoader("parameters1", "./standards/parameters1.py").load_module())
+	reader_mods = []
+	reader_mods.append(SourceFileLoader("smrMATreader", "./dataobjects/smrMATreader.py").load_module())
+	processor_mods = []
+	processor_mods.append(SourceFileLoader("processor1", "./processors/processor1.py").load_module())
+	standards_mods = []
+	standards_mods.append(SourceFileLoader("parameters1", "./standards/parameters1.py").load_module())
 	return modules
