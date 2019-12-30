91e0d34261e32318a0fae91352b944888ef05858
Jeffrey Wang <jeffreyc.wang@mail.utoronto.ca>
2019-12-30 16:58:25 -0500

    Set up parameters1

---
+++ src/standards/parameters1.py
@@ -0,0 +1,36 @@
+from importlib.machinery import SourceFileLoader
+standards = SourceFileLoader("standards", "./standards/abstract_standards.py").load_module()
+
+'''
+EEG Power Spectrum
+1-4
+5-9
+11-15
+16-25
+26+
+
+rms EMG for both channels
+ - Normalized against a "baseline" for each channel
+ - Summated into one channel
+
+Time-Frequency Balanced Spectral Entropy of EEG and EMG
+ - Time windows? Longer windows for shorter frequencies
+
+Moving Average on EEG
+
+Zero Crossings on EEG
+ - On the Moving Average
+ - On absolute Zero
+
+EMG max amplitude
+EMG average amplitude
+'''
+
+class Parameters1(standards.Standards):
+	
+	def __init__(self):
+		pass
+		
+	def validateParameters():
+		raise NotImplementedError( "No validateParameters function implemented" )	
+	
