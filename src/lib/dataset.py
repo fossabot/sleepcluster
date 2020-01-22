329a6bce8f50543f3d2b2a14fe08c8b0ce712fb2
Jeffrey Wang <jeffreyc.wang@mail.utoronto.ca>
2020-01-21 20:32:21 -0500

    Dataset.load only loads one file at a time

--- src/lib/dataset.py
+++ src/lib/dataset.py
@@ -4,28 +4,29 @@ from xlrd import open_workbook
 
 # Dataset is a collection of processed .xls files that contain feature data
 class Dataset:
-	
+
 	def __init__(self, files, features):
 		self.files = files
 		self.features = features
 		self.total_length = np.sum(self.files[:]['length'])
+		self.num_files = len(files)
 		self.num_features = len(features)
-		
+
 	def __str__(self):
 		return type(self).__name__
-		
-	def load(self):
+
+	def load(self, index):
 		np_headers = np.array([self.features])
 		data = []
-		for file in self.files:
-			book = open_workbook(file['location'])
-			sheet = book.sheet_by_index(0)
-			length = file['length']
-			for row in range(length):
-				data.append(sheet.row_slice(rowx=row,start_colx=0, end_colx=self.num_features))
+		file = self.files[index]
+		book = open_workbook(file['location'])
+		sheet = book.sheet_by_index(0)
+		length = file['length']
+		for row in range(length):
+			data.append(sheet.row_slice(rowx=row,start_colx=0, end_colx=self.num_features))
 		np_data = np.array(data)
 		return {'headers': np_headers, 'data': np_data}
-			
+
 	def write(self, location):
 		with open(location, 'w') as file:
 			file.write(str(len(self.files)) + '\n')
@@ -60,7 +61,7 @@ class Dataset:
 			for length, loc in zip(lengths, locations):
 				files.append({'location': loc, 'length': length})
 		return Dataset(files, features)
-		
+
 	@staticmethod
 	def mergeDatasets(datasets):
 		compare = datasets[0]
