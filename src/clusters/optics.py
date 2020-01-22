a68c31f29b9853e37a2d4f2ac2acfd24abaebf21
Jeffrey Wang <jeffreyc.wang@mail.utoronto.ca>
2020-01-21 20:07:28 -0500

    Implement OPTICS through sklearn

--- src/clusters/optics.py
+++ src/clusters/optics.py
@@ -1,5 +1,6 @@
 import numpy as np
 from tqdm import tqdm
+from sklearn.cluster import OPTICS
 
 from lib import data as dataLib
 from clusters import abstract_cluster as cluster
@@ -9,8 +10,12 @@ from dataobjects import dataobject as dataObject
 # A Cluster plugin that runs the OPTICS Cluster Algorithm
 class OPTICSCluster(cluster.Cluster):
 
-	def __init__(self):
-		pass
+	def __init__(self, min_samples=5, max_eps=np.inf, method='xi', eps=None, xi=0.5):
+		self.cluster = OPTICS(min_samples=min_samples, max_eps=max_eps, clustr_method=method,
+								eps=eps, xi=xi)
 
-	def cluster(self, *args, **kwargs):
-		raise NotImplementedError( "No cluster function implemented" )
+	def fit(self, data, weights=None):
+		return self.cluster.fit(data)
+
+	def predict(self, data, weights=None):
+		raise NotImplementedError( "No predict function implemented" )
