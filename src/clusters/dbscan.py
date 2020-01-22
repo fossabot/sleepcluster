cb416cbac3c23163dce11cce2afaf5766a4b8494
Jeffrey Wang <jeffreyc.wang@mail.utoronto.ca>
2020-01-21 19:57:14 -0500

    Implement DBSCAN through sklearn

--- src/clusters/dbscan.py
+++ src/clusters/dbscan.py
@@ -1,5 +1,6 @@
 import numpy as np
 from tqdm import tqdm
+from sklearn.cluster import DBSCAN
 
 from lib import data as dataLib
 from clusters import abstract_cluster as cluster
@@ -9,8 +10,11 @@ from dataobjects import dataobject as dataObject
 # A Cluster plugin that runs the DBSCAN Cluster Algorithm
 class DBSCANCluster(cluster.Cluster):
 
-	def __init__(self):
-		pass
+	def __init__(self, eps=0.5, min_samples=5):
+		self.cluster = DBSCAN(eps, min_samples)
 
-	def cluster(self, *args, **kwargs):
-		raise NotImplementedError( "No cluster function implemented" )
+	def fit(self, data, weights=None):
+		return self.cluster.fit(data, weights=weights)
+
+	def predict(self, data, weights=None):
+		raise NotImplementedError( "No predict function implemented" )
