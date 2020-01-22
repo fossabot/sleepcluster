9a8e2960645d231f9b803eef909f86890af6aab6
Jeffrey Wang <jeffreyc.wang@mail.utoronto.ca>
2020-01-22 14:42:18 -0500

    Add map class information

--- src/maps/map.py
+++ src/maps/map.py
@@ -16,7 +16,7 @@ class Map():
 			raise ValueError("Must input at least as many classes as desired")
 		maps = []
 		for permutation in list(itertools.permutations(np.arange(num_clusters))):
-			mapping = {}
+			mapping = { 'classes': self.classes }
 			for i in range(len(permutation):
 				mapping[permutation[i]] = i % self.classes
 			maps.append(mapping)
