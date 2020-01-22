073b0ef13e6192122bd3a8c16d2f48e4d7c83bca
Jeffrey Wang <jeffreyc.wang@mail.utoronto.ca>
2020-01-22 15:17:11 -0500

    Implement bestMapping

--- src/performance/abstract_performance.py
+++ src/performance/abstract_performance.py
@@ -1,38 +1,3 @@
-'''
-Performance Metric
-
-Analysis of the state model:
-	# Clusters >= 3 (For lab: 4)
-	% Ordered unambiguity (1 class, 2 class, etc)
-
-Analysis of the classifications:
-	1-1 accuracy with labels
-	Context tolerance
-
-Context:
-	Truth - Result - Tolerances
-	R - AW:
-		If up to 2 epochs before it is AW or QW and is continued by either AW or QW
-		OR
-		If up to 2 epochs after it is AW or QW
-	R - QW:
-		If up to 2 epochs before it is AW or QW and is continued by either AW or QW
-		OR
-		If up to 2 epochs after it is AW or QW
-	R - NR:
-		If up to 2 epochs before it is NR and is continued by either NR or R
-	NR - AW:
-
-
-Target consensus:
-	Priority - Take the highest priority for conflicting epochs
-	Context - Take the label that makes the most sense in Context
-	Combined - Take the best weighted Priority + Context
-'''
-# Given a mapping, labels, and targets:
-#	Calculate the performance of the labelling
-#	 - Specific to the state model (e.g. 4-state model)
-
 import numpy as np
 from abc import ABC, abstractmethod
 
@@ -49,3 +14,13 @@ class Performance(ABC):
 	@abstractmethod
 	def calculateAccuracy(self, mapping, labels, targets):
 		raise NotImplementedError("No calculateAccuracy function implemented")
+
+	def bestMapping(self, maps, labels, targets):
+		best_accuracy = -1
+		best_map = None
+		for mapping in maps:
+			accuracy = self.calculateAccuracy(mappinig, labels, targets)
+			if accuracy > best_accuracy:
+				best_accuracy = accuracy
+				best_map = mappinig
+		return best_map, best_accuracy
