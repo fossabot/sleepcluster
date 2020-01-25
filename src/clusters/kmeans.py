3c9f08be5be6d76f20d33598af0bbf1e8b712a0d
Jeffrey Wang <jeffreyc.wang@mail.utoronto.ca>
2020-01-24 21:02:21 -0500

    Fix argument naming

--- src/clusters/kmeans.py
+++ src/clusters/kmeans.py
@@ -10,14 +10,15 @@ from dataobjects import dataobject as dataObject
 # A Cluster plugin that runs the K-Means Cluster 'Elkan' Algorithm
 class KMeansCluster(cluster.Cluster):
 
-	def __init__(self, k=3, init='kmeans++', n_init=10, max_iter=100, tol=1e-4, seed=None):
-		self.cluster = KMeans(n_clusters=k, init=init, n_init=n_init, max_iter=max_iterm tol=tol, seed=seed)
+	def __init__(self, k=3, init='k-means++', n_init=10, max_iter=100, tol=1e-4, seed=None):
+		self.cluster = KMeans(n_clusters=k, init=init, n_init=n_init,
+								max_iter=max_iter, tol=tol, random_state=seed)
 
 	def fit(self, data, weights=None):
-		return self.cluster.fit(data, weights=weights)
+		return self.cluster.fit(data, sample_weight=weights)
 
 	def predict(self, data, weights=None):
-		return self.cluster.predict(data, weights=weights)
+		return self.cluster.predict(data, sample_weight=weights)
 
 	def load(self, model):
 		if model['model'] == 'kmeans':
