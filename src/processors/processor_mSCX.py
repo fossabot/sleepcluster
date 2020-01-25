77ee3fb469908cf65c460df3cbf456da40f03752
Jeffrey Wang <jeffreyc.wang@mail.utoronto.ca>
2020-01-24 21:04:41 -0500

    Implement hull moving average and log transformations in post

--- src/processors/processor_mSCX.py
+++ src/processors/processor_mSCX.py
@@ -7,13 +7,13 @@ from processors import abstract_processor as processor
 from dataobjects import dataobject as dataObject
 
 
-# A Processor plugin for the msleepclusterX-type SleepCluster Standard 
+# A Processor plugin for the msleepclusterX-type SleepCluster Standard
 class Processor_mSCX(processor.Processor):
 
 	def __init__(self, parameters):
 		self.parameters = parameters
 		pass
-		
+
 	def process(self, channels={}):
 		if channels.keys() != self.parameters.CHANNELS.keys():
 			raise RuntimeError("Channels do not comply with standards")
@@ -23,10 +23,10 @@ class Processor_mSCX(processor.Processor):
 			for el in channels[key]:
 				if not(isinstance(el, dataObject.DataObject)):
 					raise RuntimeError("Channels do not comply with standards")
-		
+
 		headers = self.formatHeaders()
 		data = []
-		
+
 		EPOCH_DATA = {}
 
 		for key in channels.keys():
@@ -34,23 +34,23 @@ class Processor_mSCX(processor.Processor):
 			for i in range(len(channels[key])):
 				channels[key][i] = channels[key][i].process(self.parameters.NORMALIZER[key], self.parameters.NORMALIZE_ARG[key])
 				EPOCH_DATA[key].append(self.calculateEpochs(channels[key][i], epoch_size=self.parameters.EPOCH_SIZE))
-		
+
 		num_epochs = []
 		for key in EPOCH_DATA.keys():
 			for channel in EPOCH_DATA[key]:
 				num_epochs.append(channel['epochs'])
 		num_epochs = min(num_epochs)
-	
+
 		for i in tqdm(range(num_epochs)):
-		
+
 			features = {}
 			for feature in self.parameters.FEATURES:
 				features[feature] = { 'merge': self.parameters.FEATURES[feature]['merge'] }
-		
+
 			for key in channels.keys():
 				for feature in features.keys():
 					features[feature][key] = []
-				
+
 				for j in range(len(channels[key])):
 					epoch = channels[key][j].getData(i*EPOCH_DATA[key][j]['size'], k=(i+1)*EPOCH_DATA[key][j]['size'])
 
@@ -59,9 +59,10 @@ class Processor_mSCX(processor.Processor):
 					noverlap = nperseg * self.parameters.NOVERLAP_FACTOR[key]
 					f, Pxx = dataLib.welch(epoch, fs=fs, nperseg=nperseg, noverlap=noverlap,
 											detrend=self.parameters.DETREND[key])
-					
+
 					if self.parameters.FEATURES['0.BANDS'][key]:
-						features['0.BANDS'][key].append(dataLib.computeBands(f, Pxx, self.parameters.FEATURES['0.BANDS'][key]))
+						bands = dataLib.computeBands(f, Pxx, self.parameters.FEATURES['0.BANDS'][key])
+						features['0.BANDS'][key].append(bands[1]/bands[0])
 					if self.parameters.FEATURES['1.ENTROPY'][key]:
 						features['1.ENTROPY'][key].append(dataLib.simpleSpectralEntropy(f, Pxx))
 					if self.parameters.FEATURES['2.RMS'][key]:
@@ -70,34 +71,44 @@ class Processor_mSCX(processor.Processor):
 						features['3.PERCENTILE'][key].append(dataLib.percentileMean(epoch, self.parameters.FEATURES['3.PERCENTILE'][key]))
 					if self.parameters.FEATURES['4.MEAN'][key]:
 						features['4.MEAN'][key].append(np.mean(epoch))
-			
-			features = self.mergeFeatures(features)	
-			data_row = [i] + self.formatFeatures(features)
+
+			features = self.mergeFeatures(features)
+			data_row = self.formatFeatures(features)
 			data.append(data_row)
 		np_data = np.array(data)
+
+		np_data = np_data.transpose()
+		for col in range(len(np_data)):
+			np_data[col] = dataLib.maxNormalize(np_data[col], scale=(5,10))
+			np_data[col] = dataLib.hullMovingAverage(np_data[col], 4)
+			np_data[col] = dataLib.logTransform(np_data[col], np.median(np_data[col]))
+		np_data = np_data.transpose()
+
 		np_headers = np.array([headers])
 		return {'headers': np_headers, 'data':np_data}
-	
+
 	def formatHeaders(self):
-		headers = ['Epoch']
+		headers = []
 		features = sorted(self.parameters.FEATURES.keys())
 		for f in features:
 			keys = sorted(self.parameters.FEATURES[f].keys())
 			for key in keys:
 				if key != 'merge' and self.parameters.FEATURES[f][key] not in [False, None]:
-					if isinstance(self.parameters.FEATURES[f][key], list):
-						for i in range(len(self.parameters.FEATURES[f][key])):
-							headers.append(f+':'+key+'-'+str(i))
+					if isinstance(self.parameters.FEATURES[f][key], list) and f == '0.BANDS':
+						headers.append(f+':'+key)
+					elif isinstance(self.parameters.FEATURES[f][key], list):
+							for i in range(len(self.parameters.FEATURES[f][key])):
+								headers.append(f+':'+key+'-'+str(i))
 					else:
 						headers.append(f+':'+key)
 		return headers
-	
+
 	def calculateEpochs(self, channel, epoch_size=5):
 		size = floor(epoch_size / channel.resolution)
 		epochs = floor(channel.length / size)
 		epoch_data = { 'size':size, 'epochs':epochs }
 		return epoch_data
-		
+
 	def mergeFeatures(self, features):
 		for f in features.keys():
 			for key in features[f].keys():
--- src/standards/msleepclusterX_0.py
+++ src/standards/msleepclusterX_0.py
@@ -4,29 +4,29 @@ from lib import data as dataLib
 # A mSleepClusterX-type SleepCluster Standard
 #	Build: 0
 class mSleepClusterX_0(standards.mSleepClusterX):
-	
+
 	def __init__(self):
 		self.CHANNELS = {
 							'EEG':1,
 							'EMG':2
 						}
 		self.EPOCH_SIZE = 5
-		
+
 		self.NORMALIZER = 		{	'EEG': dataLib.maxNormalize, 	'EMG': dataLib.maxNormalize }
 		self.NORMALIZE_ARG = 	{ 	'EEG': None, 					'EMG': None 				}
-		
+
 		self.NPERSEG_FACTOR = 	{ 	'EEG': 0.75,		'EMG': 0.75			}
 		self.NOVERLAP_FACTOR = 	{ 	'EEG': 0.5, 		'EMG': 0.5 			}
 		self.DETREND = 			{ 	'EEG': 'constant', 	'EMG': 'constant' 	}
-		
+
 		self.FEATURES = {
 							'0.BANDS':			{
-													'EEG': [	(0.5, 3), 	(7, 9), 	(12, 40)	],
+													'EEG': [	(0.5, 4), 	(7, 9), 	(11, 40)	],
 													'EMG': None,
 													'merge': 'MEAN'
 												},
-							'1.ENTROPY': 		{ 	'EEG': True,	'EMG': True, 	'merge': 'MAX' 	},
-							'2.RMS': 			{ 	'EEG': False,	'EMG': True, 	'merge': 'MAX' 	},
+							'1.ENTROPY': 		{ 	'EEG': True,	'EMG': False, 	'merge': 'MAX' 	},
+							'2.RMS': 			{ 	'EEG': False,	'EMG': False, 	'merge': 'MAX' 	},
 							'3.PERCENTILE': 	{ 	'EEG': None, 	'EMG': 95, 		'merge': 'MEAN'	},
-							'4.MEAN':			{ 	'EEG': False, 	'EMG': True, 	'merge': 'MEAN'	}
+							'4.MEAN':			{ 	'EEG': False, 	'EMG': False, 	'merge': 'MEAN'	}
 						}
