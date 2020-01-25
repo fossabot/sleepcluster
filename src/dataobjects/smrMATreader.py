3848033b0d5ac7edc19feb57b1fb3ea1e2e1285e
Jeffrey Wang <jeffreyc.wang@mail.utoronto.ca>
2020-01-24 21:02:46 -0500

    Allow score reading for target labels

--- src/dataobjects/smrMATreader.py
+++ src/dataobjects/smrMATreader.py
@@ -11,9 +11,9 @@ class smrMATreader(dataReader.DataReader):
 
 	def __init__(self, filepath):
 		super().__init__(filepath)
-		
+
 	# Read data out from specified channel in file and return a DataObject storing that data
-	def read(self, c=""):
+	def read(self, c="", score=False, map=None):
 		if c == "":
 			raise FileNotFoundError("Channel or score channel has not been specified")
 		try:
@@ -25,14 +25,25 @@ class smrMATreader(dataReader.DataReader):
 		for field in matfile.keys():
 			if '_Ch' in field:
 				channels.append(matfile[field][0][0][0][0])
-				if c == matfile[field][0][0][0][0]:
+				if score and c == matfile[field][0][0][0][0]:
 					try:
-						data = np.absolute((matfile[field][0][0][8]).flatten())
+						data = matfile[field][0][0][7].flatten()[:-1]
+						for k, v in map.items():
+							data[data == k] = v
+						data = data.astype(int)
 						resolution = matfile[field][0][0][2][0][0]
-						length = matfile[field][0][0][7][0][0]
+						length = int(matfile[field][0][0][3][0][0])
 						dataObject = dataobject.DataObject(name=c, data=data, resolution=resolution, length=length)
 					except Exception:
 						raise FileNotFoundError("An error occurred extracting from channel " + c)
+				elif c == matfile[field][0][0][0][0]:
+						try:
+							data = matfile[field][0][0][8].flatten()
+							resolution = matfile[field][0][0][2][0][0]
+							length = int(matfile[field][0][0][7][0][0])
+							dataObject = dataobject.DataObject(name=c, data=data, resolution=resolution, length=length)
+						except Exception:
+							raise FileNotFoundError("An error occurred extracting from channel " + c)
 		if data.size == 0:
 			raise FileNotFoundError("Channel named " + c + " not found. Instead found: " + str(channels))
 		return dataObject
